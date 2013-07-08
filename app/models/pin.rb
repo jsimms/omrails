e77e3c775b24ff0b46acea8622bca4f535abb8fc
Jake Simms <jakedsimms@gmail.com>
2013-07-08 18:56:00 -0400

    debug medium images everywhere

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -10,7 +10,7 @@ class Pin < ActiveRecord::Base
 
 
   belongs_to :user
-  has_attached_file :image, :styles => { :medium => "320x240"}
+  has_attached_file :image, :styles => { :medium => "320x240>"}
 
   
 
